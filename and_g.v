7bb142ba791f9c2ee7d2288fd485c019d8ff63dc
Sathya-S245 <sathyaselvam022@gmail.com>
2025-11-02 17:50:16 +0530

    Added New Logic For OR logic gate

--- and_g.v
+++ and_g.v
@@ -1,5 +1,16 @@
 module and_g(a,b,c);
 input a,b;
 output  c;
-assign c = a & b;
+assign c = a & b;// dataflow modelling
+/*
+// behavioral modelling
+always@(a or b )
+begin
+c=a &b;
+end
+
+//gatelevel modelling
+and(c,a,b);
+*/
+
 endmodule
